2fbc5357360670becdc89d26c650ed948ec1c653
arpan <arpanbatra60@gmail.com>
2024-06-05 18:52:48 +0530

    changes in product detail page

--- src/pages/product-detail.js
+++ src/pages/product-detail.js
@@ -1,124 +1,66 @@
 import { useState, useEffect } from "react";
  
-function CarSearch() {
-  const [cars, setCars] = useState([
+function ProductSearch() {
+  const [product, setProduct] = useState([
     {
-      name: "alto",
-      model: "2001",
-      color: "red",
-      price: "3 lakhs",
+      name: "Grocery",
       image:
-        "https://cdni.autocarindia.com/Utils/ImageResizer.ashx?n=https://cdni.autocarindia.com/Galleries/20141017094314_gallery1.jpg",
-      engine: "500cc",
-      fueltype: "petrol",
-      kmdriven: "10000",
-      insurance: true,
-      brand: "maruti",
+        "https://m.media-amazon.com/images/I/6131TzZaKXL._AC_UL320_.jpg",
+     
     },
     {
-      name: "thar",
-      model: "2003",
-      color: "black",
-      price: "20 lakhs",
+      name: "Mobiles",
       image:
-        "https://upload.wikimedia.org/wikipedia/commons/thumb/1/13/Mahindra_Thar_Photoshoot_At_Perupalem_Beach_%28West_Godavari_District%2CAP%2CIndia_%29_Djdavid.jpg/1280px-Mahindra_Thar_Photoshoot_At_Perupalem_Beach_%28West_Godavari_District%2CAP%2CIndia_%29_Djdavid.jpg",
-      engine: "1500cc",
-      fueltype: "petrol",
-      kmdriven: "20000",
-      insurance: true,
-      brand: "mahindra",
+        "https://m.media-amazon.com/images/I/61SmnheBBRL._AC_UY218_.jpg",
+      
     },
     {
-      name: "nano",
-      model: "2005",
-      color: "yellow",
-      price: "0.5 lakh",
+      name:"Fashion",
       image:
-        "https://upload.wikimedia.org/wikipedia/commons/thumb/2/24/Tata_Nano_Yellow.jpg/800px-Tata_Nano_Yellow.jpg",
-      engine: "800cc",
-      fueltype: "petrol",
-      kmdriven: "10000",
-      insurance: true,
-      brand: "tata",
+        "https://m.media-amazon.com/images/I/41wv42oAkVL._AC_UL320_.jpg",
+      
     },
     {
-      name: "creta",
-      model: "2024",
-      color: "white",
-      price: "18 lakhs",
+      name: "Electronics",
+     
       image:
-        "https://stimg.cardekho.com/images/car-images/930x620/Hyundai/Creta/11434/1707294369951/227_Atlas-White_e2e3e5.jpg?tr=w-898",
-      engine: "1500cc",
-      fueltype: "petrol",
-      kmdriven: "15000",
-      insurance: false,
-      brand: "hyundai",
+        "https://m.media-amazon.com/images/I/61aEl-5WVuL._AC_UY218_.jpg",
+     
     },
     {
-      name: "audi",
-      model: "2012",
-      color: "white",
-      price: "50 lakhs",
+      name:"Home & Furnitures",
       image:
-        "https://imgd-ct.aeplcdn.com/1056x660/n/cw/ec/106539/front-view1.jpeg?isig=0&wm=0&q=80",
-      engine: "1000cc",
-      fueltype: "petrol",
-      kmdriven: "30000",
-      insurance: true,
-      brand: "volkswegan",
+        "https://m.media-amazon.com/images/I/81vRCHFfZGL._AC_UL320_.jpg",
+     
     },
     {
-      name: "tiago",
-      model: "2020",
-      color: "white",
-      price: "8 lakhs",
-      image: "https://imgd-ct.aeplcdn.com/1056x660/n/8e6um4a_1527229.jpg?q=80",
-      engine: "500cc",
-      fueltype: "diesel",
-      kmdriven: "30000",
-      insurance: true,
-      brand: "tata",
+      name: "Appliances",
+     
+      image: "https://m.media-amazon.com/images/I/21wu3RPxAHL._AC_UY218_.jpg",
+     
     },
     {
-      name: "nexon",
-      model: "2020",
-      color: "white",
-      price: "10 lakhs",
-      image: "https://i.ytimg.com/vi/xbT2jcNvNZQ/maxresdefault.jpg",
-      engine: "1000cc",
-      fueltype: "petrol",
-      kmdriven: "3000",
-      insurance: true,
-      brand: "tata",
+      name: "Travel",
+    
+      image: "https://m.media-amazon.com/images/I/616bfrpGQ7L._AC_UL320_.jpg",
+    
     },
     {
-      name: "xuv 300",
-      model: "2021",
-      color: "white",
-      price: "9 lakhs",
-      image: "https://imgd.aeplcdn.com/664x374/n/01n4f0b_1627097.jpg?q=80",
-      engine: "12000 cc",
-      fueltype: "petrol",
-      kmdriven: "40000",
-      insurance: true,
-      brand: "mahindra",
+      name: "Beauty,Toys & More",
+     
+      image: "https://m.media-amazon.com/images/I/51tmWG7u5cL._AC_UL320_.jpg",
+     
     },
     {
-      name: "xuv 700",
-      model: "2022",
-      color: "white",
-      price: "20 lakhs",
+      name: "Two wheelers",
+     
       image:
-        "https://i.pinimg.com/736x/c1/46/54/c14654518e68dc1784448d8d35f7a52f.jpg",
-      engine: "20000cc",
-      fueltype: "petrol",
-      kmdriven: "5000",
-      insurance: true,
-      brand: "mahindra",
+        "https://m.media-amazon.com/images/I/71SQOhxzHlL._AC_UL640_QL65_.jpg",
+      
     },
   ]);
 
-const[selectedCar, setSelectedCar]=useState("");
+const[selectedProduct, setSelectedProduct]=useState("");
 const[searchResult, setSearchResult]=useState("");
 
   const handleSearch = () => {
@@ -132,26 +74,26 @@ const[searchResult, setSearchResult]=useState("");
   
  
   useEffect(()=>{
-    console.log(selectedCar)
-    let findCar = cars.find((element)=> element.name == selectedCar)
-    console.log(findCar)
-    setSearchResult(findCar)
+    console.log(selectedProduct)
+    let findProduct = product.find((element)=> element.name == selectedProduct)
+    console.log(findProduct)
+    setSearchResult(findProduct)
 
 
-  },[selectedCar]);
+  },[selectedProduct]);
 
   return (
     <>
       <label>
-        Select the car:
+        Select the product:
         <span>
           <select
-            name="Select the car"
-            value={selectedCar}
-            onChange={(e) => setSelectedCar(e.target.value)}
+            name="Select the product"
+            value={selectedProduct}
+            onChange={(e) => setSelectedProduct(e.target.value)}
           >
            {
-            cars.map((element, index) => {
+            product.map((element, index) => {
               return <option value={element.name}> {element.name} </option>
             })
            }
@@ -162,14 +104,9 @@ const[searchResult, setSearchResult]=useState("");
       <button type="button" onClick={reset}>Reset</button>
       <div><label>Output:</label></div>
       <div>{searchResult?.name}</div>
-      <div><label>Car Color:</label>{searchResult?.color}</div>
-      <div><label>Car Price:</label>{searchResult?.price}</div>
-      <div><label>Car Brand:</label>{searchResult?.brand}</div>
-      <div><label>Car Engine:</label>{searchResult?.engine}</div>
-      <div><label>Model:</label>{searchResult?.model}</div>
-      <div><label>insurance:</label>{searchResult?.insurance}</div>
-      <div><label>Fuel Type:</label>{searchResult?.fueltype}</div>
-      <div><label>K.M Driven:</label>{searchResult?.kmdriven}</div> 
+     
+
+     
       <div><label>Image:</label><img src={searchResult?.image}></img></div>
       
       
@@ -177,4 +114,4 @@ const[searchResult, setSearchResult]=useState("");
     </>
   );
 }
-export default CarSearch
+export default ProductSearch
