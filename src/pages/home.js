d73cecf890789e545422785b36bb8d7c38c9b8d1
arpan <arpanbatra60@gmail.com>
2024-06-06 18:48:59 +0530

    changes in various pages

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,71 +1,121 @@
 
 import { Input } from "antd";
 import { useEffect, useState } from "react";
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import { useNavigate } from "react-router-dom";
+
 
 const { Search } = Input;
 
 function ProductSearch() {
+  
+  
+
   const [search, setSearch] = useState("");
 
   const [product, setProduct] = useState([
     {
-      name: "Grocery",
-      image:
-        "https://m.media-amazon.com/images/I/6131TzZaKXL._AC_UL320_.jpg",
+      id:1,
+      name: "TechFit Smartwatch Series X",
+      image:"https://media.istockphoto.com/id/515311485/vector/smart-watch.jpg?s=612x612&w=0&k=20&c=pGojRXDoolMxGxTPdWLeynWxe0e1GqblskkI8NHwXvI=",
+    price: 199.99,
+    description: "The TechFit Smartwatch Series X is the ultimate companion for your active lifestyle...",
+    features: [
+      "Fitness Tracking, Built-in GPS, Health Monitoring",
+      
+    ],
+
+    
+ 
      
     },
     {
+      id:2,
       name: "Mobiles",
       image:
-        "https://m.media-amazon.com/images/I/61SmnheBBRL._AC_UY218_.jpg",
+        "https://rukminim2.flixcart.com/image/312/312/xif0q/mobile/c/4/d/-original-imaghx9qygjjg8hz.jpeg?q=70",
+        price:900,
+        description:"The Iphones is the ultimate companion for your active lifestyle...",
+        features:["Advanced Cameras, Apple Designed Chips, Privacy your data"
+        ],
       
     },
     {
+      id:3,
       name:"Fashion",
       image:
         "https://m.media-amazon.com/images/I/41wv42oAkVL._AC_UL320_.jpg",
+        price:30,
+        description:"The Sunglasses and other fashion item is the ultimate companion for your active lifestyle...",
+        features:"UV Protection, Lens Material, Frame Material",
       
     },
     {
-      name: "Electronics",
+      id:4,
+      name: "Cameras",
      
       image:
-        "https://m.media-amazon.com/images/I/61aEl-5WVuL._AC_UY218_.jpg",
+        "https://rukminim2.flixcart.com/image/312/312/juwzf680/dslr-camera/g/a/q/200d-ii-200d-ii-canon-original-imaffvrhzyqzayys.jpeg?q=70",
+      price:50.90,  
+      description:"The Cameras  is the ultimate companion for your active lifestyle...",
+      features:"Effective Pixels: 20.9 MP, Sensor Type: CMOS, WiFi Available, 4K",
      
     },
-    {
+    {id:5,
       name:"Home & Furnitures",
       image:
         "https://m.media-amazon.com/images/I/81vRCHFfZGL._AC_UL320_.jpg",
+      price:1000,
+      description:"10 year manufacturing warranty on the product",
+      features:"Length: 75 inch, Width: 42 inch, Thickness: 6 inch (6 ft 3 in x 3 ft 6 in x 6 in), Support Type: PU Foam, Comfort Layer: Memory Foam",  
      
     },
     {
+      id:6,
       name: "Appliances",
      
       image: "https://m.media-amazon.com/images/I/21wu3RPxAHL._AC_UY218_.jpg",
+      price:200.60,
+      description:" You can use the temperature control knob to set different temperatures for your food item based on the requirement",
+      features:"Tempered Glass Door: A double-layered tempered glass door is used for this OTG that is durable and resistant to heat. Also, the cool-touch handle ensures that your hand does not get blistered or burnt",
      
     },
     {
+      id:7,
       name: "Travel",
     
-      image: "https://m.media-amazon.com/images/I/616bfrpGQ7L._AC_UL320_.jpg",
+      image: "https://rukminim2.flixcart.com/image/612/612/xif0q/rucksack/n/y/t/-original-imagh3w9vcyusz5x.jpeg?q=70",
+      price:912,
+      description:"The travel bags is the ultimate companion for your active lifestyle...",
+      features:" Multiple pockets with zippered closures hold your belongings securely. Keep your storage organized., Adjustable straps for extra security and flexibility, Adjustable Waist Belt,",
     
     },
     {
+      id:8,
       name: "Beauty,Toys & More",
      
       image: "https://m.media-amazon.com/images/I/51tmWG7u5cL._AC_UL320_.jpg",
+      price:200,
+      description:" Introducing the Imagine Craft Mega Builder Set  a mesmerizing galaxy of creativity packed into 125+ exquisite DIY plastic building blocks, all neatly stored in a convenient storage box",
+      features:"Skillset: Attention Span Building, Analysis & Critical Thinking, Creativity & Imagination, Curiosity Building",
      
     },
     {
+      id:9,
       name: "Two wheelers",
      
       image:
         "https://m.media-amazon.com/images/I/71SQOhxzHlL._AC_UL640_QL65_.jpg",
-      
+      price:1200.90,
+      description:"Big on Lights, Big on Rides, Big on Tech, Big on Styles, Big on Colours",
+      features:"Displacement: 110.9 c, Brake Front: Drum, Tire Type: Tubeless, Under Seat Storage: 18 L",
     },
+  
   ]);
   const [filterProducts, setFilterProducts] = useState(product);
+  const navigate = useNavigate()
 
   const onSearch = (value) => {
     setSearch(value);
@@ -75,7 +125,7 @@ function ProductSearch() {
 
   useEffect(() => {
     if (search.length && count <= 20) {
-      // write filter logic and update filter car state
+      // write filter logic and update filter product state
 
       let filteredData = product.filter(function (el) {
         return el.name == search;
@@ -83,7 +133,7 @@ function ProductSearch() {
 
       setFilterProducts(filteredData);
     } else {
-      // reset to all car state
+      // reset to all product state
       setFilterProducts(product);
       if (count > 20) {
         alert("search limit can exceed");
@@ -105,22 +155,40 @@ function ProductSearch() {
         onSearch={onSearch}
       />
       <p>Search Count:{count}</p>
+      <Row md={4}>
 
-      {filterProducts.map((elem, index, arr) => {
+      {filterProducts.map((product, index, arr) => {
         return (
-          <div key={index} className="product-detail">
+          <Col onClick={() => navigate(`/product-detail/${product.id}`) } key={index} className="product-detail">
             <p>
-              <img src={elem.image}></img>
+              <img src={product.image}></img>
             </p>
             <div>
               <p>
-                <span className="title"> name : </span> {elem.name}
+                <span className="title"> name : </span> {product.name}
+              </p>
+              <p>
+                <span className="title"> price : </span> ${product.price}
+              </p>
+              <p>
+                <span className="title"> description : </span> {product.description}
+              </p>
+              <p>
+                <span className="title"> features : </span> {product.features}
               </p>
               
+             
+             
+          
+              
             </div>
-          </div>
+          </Col>
+         
+           
         );
       })}
+      </Row>
+         
     </>
   );
 }
